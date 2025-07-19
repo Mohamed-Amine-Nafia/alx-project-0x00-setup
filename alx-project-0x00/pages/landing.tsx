8341bdd8fc400ee52c6bc498f18315e512e37d2f
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-07-19 14:46:11 +0200

    update pages/landing.tsx, README.md files

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,17 @@
+import React from "react";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-8">
+      <h1 className="text-xl font-extralight mb-6">Landing Page</h1>
+
+      {/* Render Card component multiple times */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
